5eccfa582b231e1cbc89f41279dab2113fb9f11e
kisaragi22 <kisaragi22@gmail.com>
2021-07-29 10:32:35 +0900

    add New Macro

---
+++ ColorIGES.py
@@ -0,0 +1,13 @@
+# export IGES with colors
+# -*- coding: utf-8 -*-
+import ImportGui,PySide
+
+#path = FreeCAD.ConfigGet("AppHomePath")
+#path = FreeCAD.ConfigGet("UserHomePath")
+path = FreeCAD.ParamGet("User parameter:BaseApp/Preferences/General").GetString("FileOpenSavePath")
+
+SaveName, Filter = PySide.QtGui.QFileDialog.getSaveFileName(None, u"Export IGES with color", path, "*.igs")
+
+#ImportGui.export(FreeCAD.ActiveDocument.findObjects("App::Part"),SaveName)
+#ImportGui.export(FreeCAD.ActiveDocument.Objects,SaveName)
+ImportGui.export(FreeCAD.ActiveDocument.findObjects("Part::Feature"),SaveName)
